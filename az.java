da7c4003ad99430de76837ca26a2c6eb545c2681
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-07-05 20:03:09 +0530

    Create az.java

---
+++ az.java
@@ -0,0 +1,11 @@
+package BBCAjava.h_work;
+
+public class az {
+    public static void main(String[] args){
+        char c = 'a';
+        while (c <= 'z'){
+            System.out.println(c);
+            c++;
+        }
+    }
+}
