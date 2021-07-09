b75a72e19f297fdd49b65bd8df130149c95fd2fe
Shubham.Prasad <70384931+Shu6hamPrasad@users.noreply.github.com>
2021-07-09 18:35:55 +0530

    Update krishnamurthy.java

--- krishnamurthy.java
+++ krishnamurthy.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
+
 public class krishnamurthy {
-    public static int f(int n){
+    public static int f(int n) {
         int out = 1;
-        if( n == 1)
+        if (n == 1)
             return out;
-        else if(n == 0)
+        else if (n == 0)
             return 0;
-        else{
+        else {
             int i = n;
             while (i >= 1) {
                 out *= i;
@@ -15,14 +16,15 @@ public class krishnamurthy {
         }
         return out;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int k = sc.nextInt();
         int dupe = k;
         int rem;
         int fact = 0;
-        while( k != 0){
+        while (k != 0) {
             rem = k % 10;
             fact += f(rem);
             k /= 10;
